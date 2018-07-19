c3dee53d74b2071a174908ab86b233d94519f335
Tom Zhang <zhangtom54321@gmail.com>
2018-07-18 21:41:54 -0700

    Website init

--- script.php
+++
@@ -1,5 +0,0 @@
-<!-- JS -->
-<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="js/materialize.min.js"></script>
-<script type="text/javascript" src="js/typed.js"></script>
-<script type="text/javascript" src="js/main.js"></script>
